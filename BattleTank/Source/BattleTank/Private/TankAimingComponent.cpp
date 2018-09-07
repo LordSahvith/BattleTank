ed345af5ace5bd56020e5ee270efa2c54a33af57
Caleb <bulletproofchild@gmail.com>
2018-09-07 15:37:35 -0600

    using forward declarations

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,8 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "BattleTank.h"
+#include "TankBarrel.h"
+#include "TankAimingComponent.h"
 #include "kismet/GamePlayStatics.h"
 #include "Components/StaticMeshComponent.h"
-#include "TankAimingComponent.h"
 
 
 // Sets default values for this component's properties
@@ -15,7 +17,7 @@ UTankAimingComponent::UTankAimingComponent()
 	// ...
 }
 
-void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+void UTankAimingComponent::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
 	Barrel = BarrelToSet;
 }
@@ -49,10 +51,6 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
-	UE_LOG(LogTemp, Warning, TEXT("AimAsRotator: %s"), *AimAsRotator.ToString());
 
-	// move the barrel the right amount this frame
-
-
-	// given a max eleveation speed, and the time
+	Barrel->Elevate(5); // TODO REMOVE MAGIC NUMBER
 }
