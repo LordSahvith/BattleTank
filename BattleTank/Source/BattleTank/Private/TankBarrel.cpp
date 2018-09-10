35a95ec6ee9b483d92720589acddda98d2d4ea16
Caleb <bulletproofchild@gmail.com>
2018-09-09 19:29:54 -0600

    get rid of ticks

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,14 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Engine//World.h"
 #include "BattleTank.h"
 #include "TankBarrel.h"
 
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// move the barrel the right amount this frame
 	// given a max eleveation speed, and the time
-
-	UE_LOG(LogTemp, Warning, TEXT("Barrel->Elevate() called"));
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f : Barrel->Elevate() called at speed %f"), Time,  RelativeSpeed);
 }
